4c5a31d1ef9adb63cc0859990e7afe57c5074bb6
misaellandero <misaellandero@icloud.com>
2020-03-03 17:28:37 -0600

    Initial Commit

---
+++ friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  friends
+//
+//  Created by Francisco Misael Landero Ychante on 03/03/20.
+//  Copyright © 2020 Francisco Misael Landero Ychante. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
